5b0242eb38a51824b48651bfe715cdf4650d4680
XiansongLi <iametkai@gmail.com>
2019-02-25 07:10:34 -0800

    the second version

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,35 +1,49 @@
 package cse360assign2;
 
-public class Calculator 
+/**initial version*/
+public class Calculator
 {
-private int total;
-	
+	private int total;
+
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
-	
-	public int getTotal () {
-		return 0;
+	/** the getTotal method return the current total*/
+	public int getTotal () 
+	{
+		return total;
+	}
+	/**the add method add the parameter to the total variable*/
+	public void add (int value) 
+	{
+		total += value; 
+	}
+	/**the subtract method subtract the parameter from the total variable*/
+	public void subtract (int value) 
+	{
+		total -= value;
+	}
+	/**the multiply method multiply the total by the parameter*/
+	public void multiply (int value) 
+	{
+		total *= value;
+	}
+	/**the divide method divide the total by the parameter*/
+	public void divide (int value) 
+	{
+		if (value == 0)
+		{
+			total = 0;
+		}
+		else
+		{
+			total /= value;
+		}
 	}
 	
-	public void add (int value) {
-		
-	}
-	
-	public void subtract (int value) {
-		
-	}
-	
-	public void multiply (int value) {
-		
-	}
-	
-	public void divide (int value) {
-		
-	}
-	
-	public String getHistory () {
-		return "";
+	public String getHistory () 
+	{
+		return ""; 
 	}
 }
-
+  
